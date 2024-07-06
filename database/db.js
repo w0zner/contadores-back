acb5e5d54f1149bb6b69044361d3b845f0b3d4fc
César Rodrigo <cramirezf@mitic.gov.py>
2024-07-05 22:59:01 -0400

    Add database config for connection and folders to backend

---
+++ database/db.js
@@ -0,0 +1,42 @@
+const mongoose= require('mongoose')
+const admin = require('firebase-admin');
+require('dotenv').config()
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
+
+const dbMongoConection = async() => {
+    try{
+        
+        await mongoose.connect(process.env.MONGODB_STRING_CONNECTION)
+                    .then(()=> console.log('Se conecto a la Base de datos MongoDB'))
+                    .catch((err)=> console.log('MongoDB connection error ', err))
+        
+    }catch(error){
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+const dbFirebaseConection = ()=> {
+    try {
+
+        const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+          });
+          
+          const firestore = admin.firestore();
+
+          console.log('Firebase inicializado con éxito');
+
+          return firestore
+    } catch(error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = {
+    dbFirebaseConection,
+    dbMongoConection
+}
--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express= require('express')
+require('dotenv').config()
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
+const { dbFirebaseConection, dbMongoConection } = require('./database/db')
 
 //creacion del servidor
 const app= express()
 
+//const firestore = dbFirebaseConection()
+dbMongoConection()
+
 app.get('/', (req, res)=> {
     //res.send('Conexion exitosa')
+
+    //getDocument().catch(console.error);
 
     res.json({
         msg: 'Conexion exitosa'
     })
 })
 
-app.listen(5000, ()=> {
-    console.log('Conectado al puerto 5000')
+app.listen(process.env.PORT, ()=> {
+    console.log(`Conectado al puerto ${ process.env.PORT }`)
 })
+
+async function getDocument() {
+    const docRef = firestore.collection('prueba').doc('probando');
+    const doc = await docRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      console.log('Document data:', doc.data());
+    }
+  }
