e3e4d2e5645dec968e48ed7ed42cb2f39d139a7d
CESAR RODRIGO RAMIREZ FLEITAS <mcesram@SSEAF.LOCAL>
2024-07-15 16:26:57 -0400

    ajustes uploads

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,5 +1,31 @@
 const fileUpload = (req, res) => {
+    try {
+        const coleccion = req.params.coleccion
+        const id = req.params.id
 
+        const coleccionesValidas = ['documentos','usuarios']
+
+        if(!coleccionesValidas.includes(coleccion)) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'No existe la colección'
+            })
+        }
+
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'No existe el archivo a subir'
+            });
+          }
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado al subir archivo'
+        })
+    }
 }
 
 const obtenerPDF = (req, res) => {
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,12 @@
 const {Router}=require('express')
+const expressfileUpload = require('express-fileupload');
 const { validarToken } = require('../middlewares/validarjwt')
 const { fileUpload, obtenerPDF } = require('../controllers/upload')
 
+
 const router = Router()
+
+router.use(expressfileUpload());
 
 router.put('/:coleccion/:id', validarToken, fileUpload)
 router.get('/:coleccion/:pdf', obtenerPDF)
